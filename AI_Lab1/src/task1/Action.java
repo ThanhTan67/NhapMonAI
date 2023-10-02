80f9229893fb28f51ee88539b9bd8be7f26df77f
ThanhTan67 <thanhtan8900@gmail.com>
2023-10-03 00:41:21 +0700

    init project

---
+++ AI_Lab1/src/task1/Action.java
@@ -0,0 +1,8 @@
+package task1;
+
+public abstract class Action {
+	
+		public abstract boolean isNoOp();
+	}
+
+
---
+++ AI_Lab1/src/task1/NoOpAction.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class NoOpAction extends Action {
+
+	public static final NoOpAction NO_OP = new NoOpAction();
+
+	public boolean isNoOp() {
+		return true;
+	}
+
+}
